5c50e72061aaa07f499c3e9e1635b384e4ddef5b
suzmii <suzmii@foxmail.com>
2025-04-06 15:29:52 +0800

    fix(race updater): 加入重试机制

--- internal/logic/manager/cached_race.go
+++ internal/logic/manager/cached_race.go
@@ -2,113 +2,120 @@ package manager
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"github.com/suzmii/ACMBot/internal/fetcher"
-	"github.com/suzmii/ACMBot/internal/model/cache"
 	"github.com/suzmii/ACMBot/internal/model/provider"
 	"github.com/suzmii/ACMBot/internal/model/race"
-	"sort"
 	"time"
 )
 
-const cacheExp = 24 * time.Hour
-const updateExp = 5 * time.Hour
+const updateInterval = 1 * time.Minute // 每分钟检查一次
+const maxCacheAge = 4 * time.Hour      // 最大缓存时间，超过4小时需要更新
 
-var AllResource = race.AllRaceResource
-
-type CachedRace struct {
-	source   race.Resource
-	provider provider.RaceProvider
-	err      error
+var raceAndProvider = map[race.Resource]provider.RaceProvider{
+	race.ResourceCodeforces: fetcher.FetchClistCodeforcesContests,
+	race.ResourceAtcoder:    fetcher.FetchClistAtcoderContests,
+	race.ResourceLeetcode:   fetcher.FetchClistLeetcodeContests,
+	race.ResourceLuogu:      fetcher.FetchClistLuoguContests,
+	race.ResourceNowcoder:   fetcher.FetchClistNowcoderContests,
 }
 
-func (r *CachedRace) Update() error {
+// raceData 存储比赛数据及更新时间
+type raceData struct {
+	source     race.Resource
+	provider   provider.RaceProvider
+	cache      []race.Race
+	lastUpdate time.Time
+}
+
+// Update 更新比赛数据
+func (r *raceData) Update() error {
+	// 获取最新的数据
 	races, err := r.provider()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to fetch races: %v", err)
 	}
-	return cache.SetRace(r.source, races, cacheExp)
+
+	// 更新内存中的缓存数据和更新时间
+	r.cache = races
+	r.lastUpdate = time.Now()
+
+	return nil
 }
 
-func (r *CachedRace) Get() ([]race.Race, error) {
-	if r.err != nil {
-		return nil, r.err
-	}
-	return cache.GetRace(r.source)
-}
-
+// updater 用于定期更新比赛数据
 type updater struct {
-	AllCachedRace map[race.Resource]*CachedRace
+	AllCachedRace map[race.Resource]*raceData
 	UpdateTicker  *time.Ticker
 }
 
-func (r *updater) update() {
-	for _, races := range r.AllCachedRace {
-		err := races.Update()
-		if err != nil {
-			races.err = fmt.Errorf("update error: %v", err)
+// startUpdating 每分钟检查并更新比赛数据
+func (u *updater) startUpdating() {
+	u.updateAll()
+	for range u.UpdateTicker.C {
+		u.updateAll()
+	}
+}
+
+// updateAll 检查所有比赛数据的更新时间并更新
+func (u *updater) updateAll() {
+	for _, raceData := range u.AllCachedRace {
+		if time.Since(raceData.lastUpdate) > maxCacheAge {
+			logrus.Infof("Updating race %v", raceData.source)
+			err := raceData.Update()
+			if err != nil {
+				logrus.Errorf("failed to update race: %v", err)
+				continue
+			}
+			logrus.Infof("Updated race %v: data: %v", raceData.source, raceData.cache)
 		}
 	}
 }
 
-func (r *updater) get(resource race.Resource) ([]race.Race, error) {
-	races, ok := r.AllCachedRace[resource]
-	if !ok {
-		return nil, fmt.Errorf("%s not found", resource)
-	}
-	return races.Get()
-}
-
-func (r *updater) start() {
-	r.update()
-	for range r.UpdateTicker.C {
-		r.update()
-	}
-}
-
-func newUpdater(rp map[race.Resource]provider.RaceProvider, t *time.Ticker) *updater {
+// newUpdater 创建一个新的 updater 实例
+func newUpdater(rp map[race.Resource]provider.RaceProvider) *updater {
 	result := &updater{
-		AllCachedRace: make(map[race.Resource]*CachedRace),
-		UpdateTicker:  t,
+		AllCachedRace: make(map[race.Resource]*raceData),
+		UpdateTicker:  time.NewTicker(updateInterval),
 	}
 	for source, provider_ := range rp {
-		result.AllCachedRace[source] = &CachedRace{source, provider_, nil}
+		result.AllCachedRace[source] = &raceData{
+			source:     source,
+			provider:   provider_,
+			lastUpdate: time.Time{},
+		}
 	}
 	return result
 }
 
-var (
-	raceAndProvider = map[race.Resource]provider.RaceProvider{
-		race.ResourceCodeforces: fetcher.FetchClistCodeforcesContests,
-		race.ResourceAtcoder:    fetcher.FetchClistAtcoderContests,
-		race.ResourceLeetcode:   fetcher.FetchClistLeetcodeContests,
-		race.ResourceLuogu:      fetcher.FetchClistLuoguContests,
-		race.ResourceNowcoder:   fetcher.FetchClistNowcoderContests,
-	}
-	defaultUpdater = newUpdater(raceAndProvider, time.NewTicker(updateExp))
-)
+// 初始化 updater
+var defaultUpdater = newUpdater(raceAndProvider)
 
+// init 在启动时启动更新协程
 func init() {
-	go defaultUpdater.start()
+	go defaultUpdater.startUpdating()
 }
 
-func GetCachedRacesByResource(resource race.Resource) provider.RaceProvider {
+// GetRaceProviderByResource 获取指定资源的比赛数据
+func GetRaceProviderByResource(resource race.Resource) provider.RaceProvider {
 	return func() ([]race.Race, error) {
-		return defaultUpdater.get(resource)
+		raceData, ok := defaultUpdater.AllCachedRace[resource]
+		if !ok {
+			return nil, fmt.Errorf("resource %s not found", resource)
+		}
+		return raceData.cache, nil
 	}
 }
 
-func GetAllCachedRaces() ([]race.Race, error) {
+var allCachedRaceProvider = func() ([]race.Race, error) {
 	var results []race.Race
-	for _, s := range AllResource {
-		races, err := defaultUpdater.get(s)
-		if err != nil {
-			continue
-		}
-		results = append(results, races...)
+	for _, raceData := range defaultUpdater.AllCachedRace {
+		results = append(results, raceData.cache...)
 	}
-
-	sort.Slice(results, func(i, j int) bool {
-		return results[i].StartTime.Before(results[j].StartTime)
-	})
 	return results, nil
+}
+
+// GetAllCachedRaceProvider 获取所有比赛数据
+func GetAllCachedRaceProvider() provider.RaceProvider {
+	return allCachedRaceProvider
 }
