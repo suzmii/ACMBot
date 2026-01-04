e08060459143f54885fea4c76c4fdd832ba2e69b
suzmii <suzmii@foxmail.com>
2026-01-04 12:13:29 +0800

    feat: :sparkles: 添加请求超时的用户提示信息

--- middleware/errhandler.go
+++ middleware/errhandler.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"context"
 	"errors"
 
 	"github.com/suzmii/ACMBot/errorx"
@@ -12,5 +13,10 @@ func HandleError(err error) string {
 	if ok := errors.As(err, &usererr); ok {
 		return usererr.Hint
 	}
+
+	if errors.Is(err, context.Canceled) {
+		return "请求超时，再试一次？"
+	}
+
 	return "出了点问题...暂时没法处理你的请求"
 }
