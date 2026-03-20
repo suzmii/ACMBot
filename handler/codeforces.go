4a0c307f01f5bb3e3978e2359a04f6c3baa9a76e
suzmii <suzmii@foxmail.com>
2026-03-21 02:21:44 +0800

    feat: 处理异常

--- handler/codeforces.go
+++ handler/codeforces.go
@@ -82,6 +82,9 @@ func (h *Handler) GetCodeforcesRatingImage(ctx context.Context, username string)
 	// 检查更新rating
 	if time.Since(user.RatingRecords.UpdatedAt) > 4*time.Hour {
 		user, err = h.updateUserRatingRecords(ctx, user)
+		if err != nil {
+			return nil, fmt.Errorf("failed to update rating records: %w", err)
+		}
 	}
 
 	// 检查是否有rating记录
